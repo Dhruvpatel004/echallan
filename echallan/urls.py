6f4c8701cce22c141170eb7519f8ce2e75288476
Dhruvpatel004 <109230666+Dhruvpatel004@users.noreply.github.com>
2023-02-09 19:34:57 +0530

    Add files via upload

--- echallan/urls.py
+++ echallan/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', views.index,name="index"),
     path('user/', views.user,name="index"),
 
